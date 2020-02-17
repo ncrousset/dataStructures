dba53c861b617858f4d75d69da7317c903049e5b
Rudys NAtanael Acosta <natanael926@gmail.com>
2020-02-17 14:11:50 -0500

    sorted() having a key

--- list/app.py
+++ list/app.py
@@ -78,5 +78,14 @@ labelWithList(myList)
 # myList = sorted(myList, reverse=True)
 myList.sort(reverse=True)
 labelWithList(myList, "Now")
-
 print('\n')
+
+# take the second element for sort
+def take_second(elem):
+    return elem[1]
+# random list
+random = [(2, 2), (3, 4), (4, 1), (1, 3)]
+# sort list with key
+sorted_list = sorted(random, key=take_second)
+# print list
+print('Sorted list:', sorted_list)
